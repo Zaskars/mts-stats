e053d00477a95d59ef08ed37061196ac94004ebf
astrogator <timbolotov3@gmail.com>
2024-07-11 01:51:13 +0300

    refactor

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = os.getenv('DB_URL')
+DB_URL = f'postgresql+psycopg2://{os.getenv("DB_USER")}:{os.getenv("DB_PASSWORD")}@{os.getenv("DB_HOST")}:{os.getenv("DB_PORT")}/{os.getenv("DB_NAME")}'
 
 engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
