fc1255a74d90a7bb372636cffa43065d9992b9c8
astrogator <timbolotov3@gmail.com>
2024-07-10 23:57:41 +0300

    +mocked data from example

---
+++ mock_data.py
@@ -0,0 +1,106 @@
+def get_mock_data():
+    return [
+        {
+            "id": "5742",
+            "email": "zuvov@mail.ru",
+            "name": "Андрей",
+            "secondName": "Зувов",
+            "patrName": "",
+            "phone": "+74959816222",
+            "sex": "o",
+            "eventSessions": [
+                {
+                    "id": 2368927,
+                    "name": "Новое мероприятие сегодня",
+                    "startsAt": "2019-04-30T12:02:14+0300",
+                    "endsAt": "2019-04-30T13:35:52+0300",
+                    "duration": 5618,
+                    "eventId": "2308067",
+                    "questionCount": 0,
+                    "chatMessageCount": 2,
+                    "connections": [
+                        {
+                            "joined": "2019-06-21T13:28:51+0300",
+                            "leaved": "2019-04-30T12:02:47+0300",
+                            "duration": -4497964,
+                            "country": "Russia",
+                            "city": "Ulyanovsk",
+                            "platform": "Android"
+                        },
+                        {
+                            "joined": "2019-04-30T12:02:50+0300",
+                            "leaved": "2019-04-30T12:06:38+0300",
+                            "duration": 228,
+                            "country": "Russia",
+                            "city": "Ulyanovsk",
+                            "platform": "Android"
+                        }
+                    ],
+                    "userChatMessageCount": 0,
+                    "userQuestionCount": 0,
+                    "additionalFieldValues": [],
+                    "actualInvolvement": "82.29",
+                    "speechDuration": 0,
+                    "percentOfTotalSpeechDuration": "0.00",
+                    "usersReactionClicks": 0,
+                    "percentOfTotalReactionClicks": "0.00",
+                    "actualParticipantActivityPercent": 86.97,
+                    "rating": 82,
+                    "attentionControl": {
+                        "shownCount": 2,
+                        "confirmedCount": 0,
+                        "percent": 0
+                    }
+                }
+            ]
+        },
+        {
+            "id": "825893",
+            "email": "m.finogeeva@webinar.ru",
+            "name": "Мария",
+            "secondName": "Финогеева",
+            "patrName": "",
+            "phone": "89771740189",
+            "sex": "o",
+            "organization": "ООО Вебинар Технологии",
+            "position": "Руководитель группы Поддержки Вебинар",
+            "eventSessions": [
+                {
+                    "id": 2368927,
+                    "name": "Новое мероприятие сегодня",
+                    "startsAt": "2019-04-30T12:02:14+0300",
+                    "endsAt": "2019-04-30T13:35:52+0300",
+                    "duration": 5618,
+                    "eventId": "2308067",
+                    "questionCount": 0,
+                    "chatMessageCount": 2,
+                    "connections": [
+                        {
+                            "joined": "2019-04-30T12:32:14+0300",
+                            "leaved": "2019-04-30T12:35:38+0300",
+                            "duration": 204,
+                            "country": "Russia",
+                            "city": "Moscow",
+                            "platform": "iOs"
+                        }
+                    ],
+                    "userChatMessageCount": 1,
+                    "userQuestionCount": 0,
+                    "additionalFieldValues": [],
+                    "actualInvolvement": "26.42",
+                    "speechDuration": 0,
+                    "percentOfTotalSpeechDuration": "0.00",
+                    "usersReactionClicks": 0,
+                    "percentOfTotalReactionClicks": "0.00",
+                    "utms": [],
+                    "actualParticipantActivityPercent": 28.71,
+                    "rating": 26,
+                    "attentionControl": {
+                        "shownCount": 2,
+                        "confirmedCount": 2,
+                        "percent": "100,00"
+                    }
+                }
+            ]
+        }
+    ]
