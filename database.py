ebef28c4e0ac705cc647288adb9f8ec59e9aeaab
astrogator <timbolotov3@gmail.com>
2024-07-11 00:28:24 +0300

    +cascades

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_URL = os.getenv('DB_URL')
+DB_URL = 'postgresql+psycopg2://postgres:72metra@localhost:5432/mts'
 
 engine = create_engine(DB_URL)
 Session = sessionmaker(bind=engine)
